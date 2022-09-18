07198f9aa27efeecd84b1a98201ddf1b629211a2
mr-robot-007 <anujgusain108@gmail.com>
2022-09-18 12:37:07 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello clever programmer, lets's build the Amazon clone  </h1>
+      {/* header*/}
+      {/* home*/}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAJprcbXNspxhBxDXAP_GKi7N-lV9ysP1Q",
+    authDomain: "clone-d8d62.firebaseapp.com",
+    projectId: "clone-d8d62",
+    storageBucket: "clone-d8d62.appspot.com",
+    messagingSenderId: "680336315920",
+    appId: "1:680336315920:web:fa0d5c22e662982b0cdc95",
+    measurementId: "G-R7CNCP2KWX"
+  };
