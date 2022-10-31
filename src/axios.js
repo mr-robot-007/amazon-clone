6f4c8daa4601b7d93321deaa7917c1dce9592f7a
mr-robot-007 <anujgusain108@gmail.com>
2022-10-31 17:26:47 +0530

    remove the now ignored direcotory

--- src/axios.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const instance = axios.create({
-    baseURL: 'https://us-central1-clone-d8d62.cloudfunctions.net/api' 
-    // baseURL: 'http://127.0.0.1:5001/clone-d8d62/us-central1/api' 
-});
-
-export default instance;
